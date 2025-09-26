eb9f7348655a69a6b988c390f0eb644fd63d40ed
Maksel636 <axeldruyts@gmail.com>
2025-09-26 21:19:34 +0200

    platformmovescript

---
+++ dashgame/Assets/PlatformMoveScript.cs
@@ -0,0 +1,27 @@
+using System.Threading;
+using UnityEngine;
+
+public class PlatformMoveScript : MonoBehaviour
+{
+    [SerializeField] float timeToReturn;
+    private float _timer;
+    [SerializeField] float _speed;
+    private Vector3 _startPosition;
+
+    private void Awake()
+    {
+        _startPosition = transform.position;
+    }
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > (2* Mathf.PI))
+        {
+            _timer = 0;
+        }
+        float sinus = timeToReturn * Mathf.Sin(_speed*(_timer));
+        Debug.Log(sinus);
+        //Mathf.Sin()
+        transform.position = _startPosition + new Vector3(sinus, 0, 0);
+    }
+}
