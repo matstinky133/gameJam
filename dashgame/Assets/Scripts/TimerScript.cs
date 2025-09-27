58f98eba5f55aec22c99a762d939f2a69093bed7
Maksel636 <axeldruyts@gmail.com>
2025-09-27 11:17:25 +0200

    timer

--- dashgame/Assets/Scripts/TimerScript.cs
+++ dashgame/Assets/Scripts/TimerScript.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float _timer = 0;
+    [SerializeField] private float TimeToExplode = 5f;
+    [SerializeField] private Image _image;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _positionUpValue;
+
     void Start()
     {
-        
+        _image = GetComponent<Image>();
+        _timer = TimeToExplode;    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Camera.main.WorldToScreenPoint(_player.transform.position+ _positionUpValue *Vector3.up);
+        _timer -= Time.deltaTime;
+        if(_timer < 0)
+        {
+            _timer = 0;
+        }
+
+
+        _image.fillAmount = _timer/TimeToExplode;
+
+       
+
+
     }
 }
