2f572d85546ea833ccc612de328e055cb062cb00
Maksel636 <axeldruyts@gmail.com>
2025-09-27 11:54:24 +0200

    Timer

--- dashgame/Assets/Scripts/TimerScript.cs
+++ dashgame/Assets/Scripts/TimerScript.cs
@@ -1,34 +1,55 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
-    [SerializeField] private float _timer = 0;
-    [SerializeField] private float TimeToExplode = 5f;
-    [SerializeField] private Image _image;
+
+
+    [Header("timers")]
+    [SerializeField] private float _timerPersonal = 0;
+    [SerializeField] private float _timenotTagged = 0;
+
+    [Header("Max time")]
+    [SerializeField] private float _timeToExplode = 5f;
+    [SerializeField] private float _baseTimeToExplode = 5f;
+    [SerializeField] private float _reverseSpeedToReduceTimer = 10f;
+
+    [SerializeField] private float _totalTime = 60f;
+
+    [Header("props")]
+    [SerializeField] private Image _visualTimer;
     [SerializeField] private GameObject _player;
     [SerializeField] private float _positionUpValue;
-
+    //[SerializeField] private TextMeshProUGUI _digitalClock;
     void Start()
     {
-        _image = GetComponent<Image>();
-        _timer = TimeToExplode;    
+        _visualTimer = GetComponent<Image>();
+        _timerPersonal = _timeToExplode;    
+        _timenotTagged = _totalTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        _timeToExplode =   _baseTimeToExplode / (_timenotTagged/ _reverseSpeedToReduceTimer);
+
+        _timenotTagged += Time.deltaTime;
+        //PersonalTimer
         transform.position = Camera.main.WorldToScreenPoint(_player.transform.position+ _positionUpValue *Vector3.up);
-        _timer -= Time.deltaTime;
-        if(_timer < 0)
+        _timerPersonal -= Time.deltaTime;
+        if(_timerPersonal < 0)
         {
-            _timer = 0;
+            _timerPersonal = _timeToExplode;
         }
+        _visualTimer.fillAmount = _timerPersonal/_timeToExplode;
 
+        //digitalTimer;
+        
+        
 
-        _image.fillAmount = _timer/TimeToExplode;
-
-       
+        //_digitalClock.text = $"{_timenotTagged}";
 
 
     }
