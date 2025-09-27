3454537687b42986d586ab44da6c43e542b02d7c
matstinky133 <letsplaystufz@gmail.com>
2025-09-27 12:02:57 +0200

    fixed jumppads

--- dashgame/Assets/Scripts/JumpPadScript.cs
+++ dashgame/Assets/Scripts/JumpPadScript.cs
@@ -6,22 +6,39 @@ public class JumpPadScript : MonoBehaviour
     [SerializeField] private float _force = 1f;
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //  if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        Rigidbody2D rb = collision.rigidbody;
+
+    //        // rb.totalForce = new Vector2(rb.totalForce.x, 0);
+
+    //        rb.totalForce = Vector2.zero;
+    //        rb.AddRelativeForce(Vector2.up * _force, ForceMode2D.Impulse);
+            
+    //        Debug.DrawLine(collision.transform.position, collision.transform.position + new Vector3(0, (rb.GetRelativePointVelocity(collision.transform.position).y), 0), Color.red, 1);
+
+    //        Debug.Log(rb.GetRelativePointVelocity(collision.transform.position));
+    //    }
+        
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-      if (collision.gameObject.CompareTag("Player"))
+        if(collision.CompareTag("Player"))
         {
-            Rigidbody2D rb = collision.rigidbody;
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
 
             // rb.totalForce = new Vector2(rb.totalForce.x, 0);
 
-            rb.totalForce = Vector2.zero;
+            rb.linearVelocityY = 0;
             rb.AddRelativeForce(Vector2.up * _force, ForceMode2D.Impulse);
-            
+
             Debug.DrawLine(collision.transform.position, collision.transform.position + new Vector3(0, (rb.GetRelativePointVelocity(collision.transform.position).y), 0), Color.red, 1);
 
             Debug.Log(rb.GetRelativePointVelocity(collision.transform.position));
         }
-        
     }
-    
+
 }
